bbb3e394ca44e12868962b655b327dff77ab55ae
Daniel Sanchez <ds@dseehappy>
2024-03-18 23:00:20 -0600

    image and theming cta3

--- src/app/components/cta3.tsx
+++ src/app/components/cta3.tsx
@@ -7,6 +7,7 @@ import {
     LockClosedIcon,
     ServerIcon,
 } from '@heroicons/react/20/solid'
+import Image from "next/image";
 
 const features = [
     {
@@ -46,9 +47,9 @@ export default function Cta3() {
         <div className="bg-white py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl sm:text-center">
-                    <h2 className="text-base font-semibold leading-7 text-indigo-600">Everything you need</h2>
-                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">No server? No problem.</p>
-                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                    <h2 className="text-base font-semibold leading-7 text-brand-dark-orange">Everything you need</h2>
+                    <p className="mt-2 text-3xl font-bold tracking-tight text-brand-dark-blue sm:text-4xl">No server? No problem.</p>
+                    <p className="mt-6 text-lg leading-8 text-brand-light-orange">
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste
                         dolor cupiditate blanditiis.
                     </p>
@@ -56,8 +57,8 @@ export default function Cta3() {
             </div>
             <div className="relative overflow-hidden pt-16">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                    <img
-                        src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
+                    <Image
+                        src="/img/High-Views-Seamless-Gutters-Denver-CO-Gutter-Guard-Installation.jpg"
                         alt="App screenshot"
                         className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
                         width={2432}
@@ -72,11 +73,11 @@ export default function Cta3() {
                 <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
                     {features.map((feature) => (
                         <div key={feature.name} className="relative pl-9">
-                            <dt className="inline font-semibold text-gray-900">
-                                <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600" aria-hidden="true" />
+                            <dt className="inline font-semibold text-brand-dark-blue">
+                                <feature.icon className="absolute left-1 top-1 h-5 w-5 text-brand-dark-orange" aria-hidden="true" />
                                 {feature.name}
                             </dt>{' '}
-                            <dd className="inline">{feature.description}</dd>
+                            <dd className="inline text-brand-light-blue">{feature.description}</dd>
                         </div>
                     ))}
                 </dl>
